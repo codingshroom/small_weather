24f81d3497fc21037203c171f47305672b919d35
codingshroom <194294436+codingshroom@users.noreply.github.com>
2026-06-05 09:43:48 +0200

    fixing formatting mistakes in src/database.py

--- src/database.py
+++ src/database.py
@@ -1,22 +1,33 @@
 import sqlite3
 
 
-def insert_into(connection, table, types, values):
+def insert_into(connection, table, columns, values):
+    columns_as_string = ", ".join(c for c in columns)
+    qmarks = ", ".join("?" for _ in columns)
     statement = f"""
-        INSERT INTO {table} ({[type for type in types]})
-        VALUES ({["?, " for _ in types]})
+        INSERT INTO {table} ({columns_string})
+        VALUES ({qmarks})
         """
     cursor.execute(statement, tuple(values))
     connection.commit()
 
 
 def select_from(table, columns=["*"], condition=1):
+    columns_as_string = ", ".join(c for c in columns)
     statement = f"""
-        SELECT {[column for column in columns]}
-        FROM table
+        SELECT {columns_as_string}
+        FROM {table}
         WHERE {condition}
     """
     cursor.execute(statement)
     rows = cursor.fetchall()
     return rows
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
 
