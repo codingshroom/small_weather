5d616ecd21035298105e560cce4e74c5b2ba3350
codingshroom <194294436+codingshroom@users.noreply.github.com>
2026-07-01 15:32:16 +0200

    improving src/check_request.py

--- src/check_request.py
+++ src/check_request.py
@@ -1,34 +1,38 @@
 import sqlite3
 
 from src.database import select_from, insert_into
+from apis.coordinates_api import coordinates_api_call
 
 
 '''
     How to use data_exists():
 
-    cities:         cityName
-    dates:          date
-    moonphases:     stage, illumination
-    profiles:       profileName
-    requests:       profileID, cityID, date
-    weatherdata:    cityID, date, hour, temperature, rainProbability, rainAmount
+    tablename           **kwargs
+    ------------------------------
+    cities:             cityName
+    dates:              date
+    moonphases:         stage, illumination
+    profiles:           profileName
+    requests:           profileID, cityID, date
+    weatherdata:        cityID, date, hour, temperature, rainProbability, rainAmount
 '''
-
-
 def data_exists(cursor, connection, table, **data):
     condition_string = " AND ".join(f"{key} = '{value}'" for key, value in data.items())
     exists = select_from(cursor, connection, table, columns=["1"], condition=condition_string)
     return bool(exists)
 
 
-def test_blank(table, **data):
+def prep_city(city):
+    lat, lon = coordinates_api_call(city)
+
+
+def request_data(table, **data):
     try:
         with sqlite3.connect("data/test.db") as connection:
             connection.execute("PRAGMA foreign_keys = ON")
             cursor = connection.cursor()
 
             exists = data_exists(cursor, connection, table, **data)
-            print(exists)
 
             if not exists:
                 columns = [key for key, _ in data.items()]
@@ -36,14 +40,15 @@ def test_blank(table, **data):
                 insert_into(cursor, connection, table, columns, values)
 
             rows = select_from(cursor, connection, table)
-            print(rows)
+            return rows
 
     except sqlite3.OperationalError as e:
         print("Failed to open database", e)
 
 
 def main():
-    test_blank("weatherdata", cityID=2, date="2026-06-30", hour=1, temperature=21, rainProbability=60, rainAmount=4)
+    rows = request_data("weatherdata", cityID=2, date="2026-06-30", hour=3)
+    print(rows)
 
 
 if __name__ == "__main__":
