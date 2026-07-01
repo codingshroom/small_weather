754f51b14030417f3b0e0df89a5f29c5139f939a
codingshroom <194294436+codingshroom@users.noreply.github.com>
2026-07-01 12:30:27 +0200

    refactored src/check_request.py into two functions

--- src/check_request.py
+++ src/check_request.py
@@ -3,185 +3,47 @@ import sqlite3
 from src.database import select_from, insert_into
 
 
-def check_profile(cursor, connection, profile_name):
-    if not select_from(cursor, connection, "profiles", columns=["1"], condition=f"profileName = '{profile_name}'"):
-        return False
-    return True
+'''
+    How to use data_exists():
+
+    cities:         cityName
+    dates:          date
+    moonphases:     stage, illumination
+    profiles:       profileName
+    requests:       profileID, cityID, date
+    weatherdata:    cityID, date, hour, temperature, rainProbability, rainAmount
+'''
 
 
-def check_moonphase(cursor, connection, stage, illumination):
-    stage_exists = select_from(cursor, connection, "moonphases", columns=["1"], condition=f"stage = '{stage}'") 
-    illumination_exists = select_from(cursor, connection, "moonphases", columns=["1"], condition=f"illumination = '{illumination}'") 
-
-    if stage_exists and illumination_exists:
-        return True
-    return False
+def data_exists(cursor, connection, table, **data):
+    condition_string = " AND ".join(f"{key} = '{value}'" for key, value in data.items())
+    exists = select_from(cursor, connection, table, columns=["1"], condition=condition_string)
+    return bool(exists)
 
 
-def check_city(cursor, connection, city_name):
-    if not select_from(cursor, connection, "cities", columns=["1"], condition=f"cityName = '{city_name}'"):
-        return False
-    return True
-
-
-def check_date(cursor, connection, date):
-    if not select_from(cursor, connection, "dates", columns=["1"], condition=f"date = '{date}'"):
-        return False
-    return True
-
-
-def check_request(cursor, connection, profileID, cityID, date):
-    if not select_from(cursor, connection, "requests", columns=["1"], condition=f"profileID = '{profileID}' AND cityID = '{cityID}' AND date = '{date}'"):
-        return False
-    return True
-
-
-def check_weatherdata(cursor, connection, cityID, date, hour):
-    if not select_from(cursor, connection, "weatherdata", columns=["1"], condition=f"cityID = '{cityID}' AND date = '{date}' AND hour = '{hour}'"):
-        return False
-    return True
-
-
-def test_weatherdata():
-    cityID = 1
-    date = "2026-07-01"
-    hour = 5
-    temp = 15
-    rainProb = 42
-    rainAmount = 0.5
-
+def test_blank(table, **data):
     try:
         with sqlite3.connect("data/test.db") as connection:
             connection.execute("PRAGMA foreign_keys = ON")
             cursor = connection.cursor()
 
-            weatherdata_exists = check_weatherdata(cursor, connection, cityID, date, hour)
-            print(weatherdata_exists)
+            exists = data_exists(cursor, connection, table, **data)
+            print(exists)
 
-            if not weatherdata_exists:
-                insert_into(cursor, connection, "weatherdata", ["cityID", "date", "hour", "temperature", "rainProbability", "rainAmount"], [cityID, date, hour, temp, rainProb, rainAmount])
+            if not exists:
+                columns = [key for key, _ in data.items()]
+                values = [value for _, value in data.items()]
+                insert_into(cursor, connection, table, columns, values)
 
-            rows = select_from(cursor, connection, "weatherdata")
+            rows = select_from(cursor, connection, table)
             print(rows)
 
     except sqlite3.OperationalError as e:
         print("Failed to open database", e)
-
-
-def test_request():
-    date = "2026-07-01"
-    profileID = 1
-    cityID = 1
-
-    try:
-        with sqlite3.connect("data/test.db") as connection:
-            connection.execute("PRAGMA foreign_keys = ON")
-            cursor = connection.cursor()
-
-            request_exists = check_request(cursor, connection, profileID, cityID, date)
-            print(request_exists)
-
-            if not request_exists:
-                insert_into(cursor, connection, "requests", ["profileID", "cityID", "date"], [profileID, cityID, date])
-
-            rows = select_from(cursor, connection, "requests")
-            print(rows)
-
-    except sqlite3.OperationalError as e:
-        print("Failed to open database", e)
-
-
-def test_date():
-    date = "2026-07-01"
-    moonID = 1
-
-    try:
-        with sqlite3.connect("data/test.db") as connection:
-            connection.execute("PRAGMA foreign_keys = ON")
-            cursor = connection.cursor()
-
-            date_exists = check_date(cursor, connection, date)
-            print(date_exists)
-
-            if not date_exists:
-                insert_into(cursor, connection, "dates", ["date", "moonID"], [date, moonID])
-
-            rows = select_from(cursor, connection, "dates")
-            print(rows)
-
-    except sqlite3.OperationalError as e:
-        print("Failed to open database", e)
-
-
-def test_city():
-    city = "Tokyo"
-    lat = 3
-    lon = 21
-
-    try:
-        with sqlite3.connect("data/test.db") as connection:
-            connection.execute("PRAGMA foreign_keys = ON")
-            cursor = connection.cursor()
-
-            city_exists = check_city(cursor, connection, city)
-            print(city_exists)
-
-            if not city_exists:
-                insert_into(cursor, connection, "cities", ["cityName", "latitude", "longitude"], [city, lat, lon])
-
-            rows = select_from(cursor, connection, "cities")
-            print(rows)
-
-    except sqlite3.OperationalError as e:
-        print("Failed to open database", e)
-
-
-def test_profile():
-    profile = "Kel"
-
-    try:
-        with sqlite3.connect("data/test.db") as connection:
-            connection.execute("PRAGMA foreign_keys = ON")
-            cursor = connection.cursor()
-
-            profile_exists = check_profile(cursor, connection, profile)
-            print(profile_exists)
-
-            if not profile_exists:
-                insert_into(cursor, connection, "profiles", ["profileName"], [profile])
-
-            rows = select_from(cursor, connection, "profiles")
-            print(rows)
-
-    except sqlite3.OperationalError as e:
-        print("Failed to open database", e)
-
-
-def test_moonphase():
-    stage = "waxing"
-    illumination = "34"
-
-    try:
-        with sqlite3.connect("data/test.db") as connection:
-            connection.execute("PRAGMA foreign_keys = ON")
-            cursor = connection.cursor()
-
-            moonphase_exists = check_moonphase(cursor, connection, stage, illumination)
-            print(moonphase_exists)
-
-            if not moonphase_exists:
-                insert_into(cursor, connection, "moonphases", ["stage", "illumination"], [stage, illumination])
-
-            rows = select_from(cursor, connection, "moonphases")
-            print(rows)
-
-    except sqlite3.OperationalError as e:
-        print("Failed to open database", e)
-
 
 
 def main():
-    test_weatherdata()
+    test_blank("weatherdata", cityID=2, date="2026-06-30", hour=1, temperature=21, rainProbability=60, rainAmount=4)
 
 
 if __name__ == "__main__":
