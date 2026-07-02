bddd406fad65da2ef7a3027da7e3de51c6513472
codingshroom <194294436+codingshroom@users.noreply.github.com>
2026-07-02 14:19:03 +0200

    wrote function signatures for src/check_request.py

--- src/check_request.py
+++ src/check_request.py
@@ -6,29 +6,57 @@ from apis.moon_api import moon_api_call
 from apis.weather_api import weather_api_call
 
 
-def request_data(table, lookup_data):
-    pass
+def open_database():
+    try:
+        with sqlite3.connect("data/database.db") as connection:
+            connection.execute("PRAGMA foreign_keys = ON")
+            cursor = connection.cursor()
+            return cursor, connection
+
+    except sqlite3.OperationalError as e:
+        print("Failed to open database", e)
+        return None, None
 
 
-def fetch_missing_data(table, lookup_data):
-    pass
+def request_data(table, profile_name, city_name, date):
+    profileID = get_profile(profile_name)
+    cityID = get_city(city_name)
+    moonphase = get_moon(date)
+    weather_data = get_weather(cityID, date)
+    store_request(profileID, cityID, date)
+    return joined_data
 
 
-def fetch_city(cityname):
-    pass
+def get_profile(profile_name)
+    # check DB for profile_name, if missing: insert into table 'profiles', return profileID
+    return profileID
 
 
-def fetch_moon(date):
-    pass
+def get_city(city_name):
+    # check DB for cityname, if missing: call coordinates_api, insert city, lat, lon to table 'cities', return cityID
+    return cityID
 
 
-def fetch_weather(cityname, date):
-    pass
+def get_moon(date):
+    # check DB via date, if missing: call moon_api, insert date + moonphase into dates & moonphases respectively, return moon_data
+    return moon_data
+
+
+def get_weather(cityID, date):
+    # check DB for weather_data, if missing: get coordinates from table 'cities', call weather_api, return requested weather_data for city + date
+    return weather_data
+
+
+def store_request(profile, cityID, date):
+    # insert request into table 'requests' if not already contained
+    return
 
 
 
 def main():
-    pass
+    cursor, connection = open_database()
+    requested_data = request_data("cities", "Johnny", "Berlin", "2026-06-30")
+    print(requested_data)
 
 
 if __name__ == "__main__":
